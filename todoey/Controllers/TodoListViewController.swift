14459c22107dd12473117c0d3db3b84580816006
Gene K. Essel <eugene2essel@gmail.com>
2018-03-29 19:55:10 -0400

    CoreData CRUD

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreData
+
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,7 +46,6 @@ class TodoListViewController: UITableViewController {
         
         //Ternary operator ==>
         //value = condition ? valueIfTrue : valueIfFalse
-        
        cell.accessoryType = item.done  ? .checkmark : .none
 
         return cell
@@ -52,6 +54,11 @@ class TodoListViewController: UITableViewController {
     //MARK - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print (itemArray[indexPath.row])
+        
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
@@ -68,8 +75,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks the Add Item to alert
-            let newItem = Item()
+            
+            
+            let newItem = Item(context: self.context)
             newItem.title = textfield.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -87,28 +97,24 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Model Manipulation Methods
     func saveItems() {
-        let encorder = PropertyListEncoder()
+        
         do{
-            let data = try encorder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         }catch {
-            print("Error encoding item array, \(error)")
+           print("Error saving context \(error)")
         }
         self.tableView.reloadData()
 
     }
     
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-                
-            }catch {
-                print("Error decoding files, \(error)")
-            }
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+        itemArray = try context.fetch(request)
+        }catch {
+            print("Error fetching data from context \(error)")
         }
-    }
+   }
     
 }
 
